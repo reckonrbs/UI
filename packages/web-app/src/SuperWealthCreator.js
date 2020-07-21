2af33f2cf3b0f54af37198d7b5b353eea944d283
Your Name <you@example.com>
2020-07-21 22:48:41 +0530

    sumit changes - version 1.1

--- packages/web-app/src/SuperWealthCreator.js
+++ packages/web-app/src/SuperWealthCreator.js
@@ -8,17 +8,18 @@ import {
     Accordion,
     Button,
     Alert,
+    Carousel,
 } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import './SuperWealthCreator.css'
 import { useDispatch, useSelector } from 'react-redux'
-import {
-    getAccountList,
-    getAccountRecommendations,
-} from '@openbanking/ui-data/lib/services/account-service'
-import InfoDisplay from '@openbanking/ui-common/lib/InfoDisplay'
 
 const SuperWealthCreator = () => {
+    // useEffect(() => {
+    //     // Update the document title using the browser API
+    //     document.title = `You clicked ${count} times`;
+    //   },[]);
+
     const data = useSelector((state) => state.app.data)
     const accountId = useSelector((state) => state.account.accountId)
 
@@ -32,7 +33,94 @@ const SuperWealthCreator = () => {
         ['Loan', 2],
     ]
 
+    const bankAccount =
+        //state is by default an object
+        [
+            { id: 1, balance: 10 },
+            { id: 2, balance: 20 },
+            { id: 3, balance: 30 },
+            { id: 4, balance: 40 },
+            { id: 5, balance: 50 },
+        ]
+
+    const recomendation =
+        //state is by default an object
+        {
+            'Covid19#100': {
+                ProductName: 'covid19Product',
+                ProductId: 'Covid19#100',
+                ProductType: 'Health',
+                roi: 3.14,
+                additionalBenifits: 'additionalBenifits',
+            },
+            'CancerTreatment#100': {
+                ProductName: 'CancerTreatmentProduct',
+                ProductId: 'CancerTreatment#100',
+                ProductType: 'Health',
+                roi: 3.5,
+                additionalBenifits: 'additionalBenifits',
+            },
+            'HeartTreatment#100': {
+                ProductName: 'HeartTreatmentProduct',
+                ProductId: 'HeartTreatment#100',
+                ProductType: 'Health',
+                roi: 4.5,
+                additionalBenifits: 'additionalBenifits',
+            },
+        }
+
+    const renderTableData = () => {
+        return bankAccount.map((bankAccount, index) => {
+            const { id, balance } = bankAccount //destructuring
+            return (
+                //   <tr key={id}>
+                //      <td>{id}</td>
+                //      <td>{balance}</td>
+                //   </tr>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://www.iconsdb.com/icons/preview/black/square-xxl.png"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Bank account - {id}</h3>
+                        <p>Balance- {balance}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            )
+        })
+    }
+
+    const renderAccordionData = () => {
+        return Object.keys(recomendation).map((key, index) => {
+            if (recomendation.hasOwnProperty(key)) {
+                const { ProductName, ProductId, roi } = recomendation[key]
+                return (
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey={index + 1}
+                            >
+                                {ProductName}
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey={index + 1}>
+                            <Card.Body>
+                                Hello! Know more and opt for {ProductName} with
+                                id = {ProductId} having ROI = {roi}
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                )
+            }
+        })
+    }
+
     var bankCheck = mostTransacted === 'Banking'
+
     return (
         <Container fluid>
             <Alert variant="success">
@@ -41,13 +129,24 @@ const SuperWealthCreator = () => {
                 </Alert.Heading>
             </Alert>
 
+            {/* <div>
+            <table id='bankAccountsTable'>
+               <tbody>
+               <tr>
+    <td>Account ID</td>
+    <td>balance</td>
+  </tr><tr>
+                  {renderTableData()}</tr>
+               </tbody>
+            </table>
+         </div> */}
+
             <div className="content_body">
                 <h5>Hi, Customer !</h5>
                 <br />
+                <Carousel>{renderTableData()}</Carousel>
                 <br />
-                {/* <Row>
-                <Col><p>One year Analaysis  </p></Col>
-    </Row> */}
+
                 <Row>
                     <Col>
                         {' '}
@@ -79,38 +178,7 @@ const SuperWealthCreator = () => {
                         </p>
                         <br />
                         {console.log(bankCheck)}
-                        <Accordion defaultActiveKey="0">
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle
-                                        as={Button}
-                                        variant="link"
-                                        eventKey="0"
-                                    >
-                                        Saving plan for x years
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                    <Card.Body>Hello! I'm the body</Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                            <Card>
-                                <Card.Header>
-                                    <Accordion.Toggle
-                                        as={Button}
-                                        variant="link"
-                                        eventKey="0"
-                                    >
-                                        Plan 2
-                                    </Accordion.Toggle>
-                                </Card.Header>
-                                <Accordion.Collapse eventKey="0">
-                                    <Card.Body>
-                                        Hello! I'm another body
-                                    </Card.Body>
-                                </Accordion.Collapse>
-                            </Card>
-                        </Accordion>
+                        <Accordion>{renderAccordionData()}</Accordion>
                     </Col>
                 </Row>
             </div>
